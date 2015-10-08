e2aef9aec0af2f778f055935f20f993273c958ea
Brian Fitzpatrick <brian@froxen.com>
2015-10-07 21:18:58 -0500

    Initial commit

---
+++ smartapps/b-fitzpatrick/contact-sensor-left-open.src/contact-sensor-left-open.groovy
@@ -0,0 +1,64 @@
+/**
+ *  Contact Sensor Left Open
+ *
+ *  Copyright 2015 Brian Fitzpatrick
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Contact Sensor Left Open",
+    namespace: "b-fitzpatrick",
+    author: "Brian Fitzpatrick",
+    description: "Notifies is a contact sensor is left open for a period of time.",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Settings") {
+		input "sensor", "capability.contactSensor", title: "Which sensor?", multiple: false
+        input "timeout", "number", title: "Notify if open how many minutes?"
+        input "message", "text", title: "Notification text"
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+    unschedule()
+	initialize()
+}
+
+def initialize() {
+	subscribe(sensor, "contact.open", openHandler)
+    subscribe(sensor, "contact.closed", closedHandler)
+}
+
+def openHandler(evt) {
+	runIn(60*timeout, timeoutHandler)
+}
+
+def timeoutHandler(evt) {
+	sendPush(message)
+}
+
+def closedHandler(evt) {
+	unschedule()
+}
